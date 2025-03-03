2e47ef4bf8decb6d161eae4acbe32597e37eceb0
Ron Courville <nova1248@gmail.com>
2025-03-03 12:45:39 -0800

    Adding analytics

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,5 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google"
-
+import { Analytics } from "@vercel/analytics/react"
 import "@workspace/ui/styles/globals.css"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 
